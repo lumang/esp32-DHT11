4ba48e9feabea502c58bd1481f1399ad34208221
lumang <luguannan0107@126.com>
2024-03-01 12:15:57 +0800

    Update simple_read.c
    update delay function

--- examples/simple_read.c
+++ examples/simple_read.c
@@ -10,18 +10,18 @@
 #include "freertos/task.h"
 #include "driver/gpio.h"
 #include "sdkconfig.h"
+#include <stdio.h>
 
-#include "waiter.h"
 #include "dht11.h"
 
 void app_main()
 {
-    DHT11_init(GPIO_NUM_4);
+    DHT11_init(GPIO_NUM_4);// modify GPIO_NUM
 
     while(1) {
         printf("Temperature is %d \n", DHT11_read().temperature);
         printf("Humidity is %d\n", DHT11_read().humidity);
         printf("Status code is %d\n", DHT11_read().status);
-        waitSeconds(1);
+        vTaskDelay(1000 / portTICK_RATE_MS);
     }
 }
